1c97506941c74c9b239bac5638ce4f83d045c77a
Blake Thomas <beebop@baz.com>
2015-05-01 15:32:38 -0700

    Added slightly more debug info

--- kernel.c
+++ kernel.c
@@ -9,7 +9,6 @@ void kernel_main() {
   setup_idt();
   install_exc_handlers();
   init_irq();
-  init_keyboard();
   vga_init();
   vga_setcolor(vga_color(COLOR_RED, COLOR_BLACK));
   vga_write("Fire");
@@ -17,6 +16,8 @@ void kernel_main() {
   vga_write("OS");
   vga_setcolor(vga_color(COLOR_LIGHT_GREY, COLOR_BLACK));
   vga_write(" version " VERSION " initialized\n");
+  init_keyboard();
+  vga_write("Keyboard initialized\n");
   while(1) {
     char c = getc();
     vga_putchar(c);
